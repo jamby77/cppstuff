700f6142c4d9e1d20863c4e4feceb4fbc4520592
Petar Dzhambazov <jamby77@gmail.com>
2018-03-21 22:46:09 +0200

    added wallet and transactions code

---
+++ transacitons.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#define TRANSACTIONS_FILE_NAME "transactions.dat"
+struct Transaction
+{
+    long long time;
+    unsigned senderId;
+    unsigned receiverId;
+    double fmiCoins;
+};
+
+const long HOLLY_MOTHER_ACCOUNT = 4294967295;
+const unsigned short FMICOIN_RATE = 375;
+
+void initialTransaction(unsigned walletId, double fiatMoney);
--- wallet.h
+++ wallet.h
@@ -1,1 +1,12 @@
+#include <string>
+#define WALLET_FILE_NAME "wallets.dat"
+const size_t MAXN = 256;
+
+struct Wallet
+{
+    char owner[MAXN];
+    unsigned id;
+    double fiatMoney;
+};
+
 void addWallet(int argc, char *argv[]);
