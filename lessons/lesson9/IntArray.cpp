0d770bd0925a6323cabdee6035b1752c98f6e67f
Petar Dzhambazov <pdzhambazov@vayant.com>
2018-07-20 11:07:03 +0300

    lesson 10.6

--- lessons/lesson9/IntArray.cpp
+++ lessons/lesson9/IntArray.cpp
@@ -4,12 +4,18 @@
 
 #include <cassert>
 #include "IntArray.h"
+void IntArray::init(int size) {
+  assert(size >= 0 && "IntArray: size should be more than 0");
+  if (size == 0)
+    m_int = nullptr;
+  else
+    m_int = new int[size];
+}
 IntArray::IntArray(int size) : m_size{size} {
-  assert(size > 0 && "IntArray: size should be more than 0");
-  m_int = new int[size];
+  init(m_size);
 }
 int &IntArray::operator[](const int idx) {
-  assert(idx < m_size && "IntArray: subscript index too large");
+  assertIndex(idx);
   return m_int[idx];
 }
 std::ostream &operator<<(std::ostream &out, const IntArray &ia) {
@@ -44,4 +50,68 @@ IntArray::IntArray(const IntArray &ia) : m_size{ia.m_size} {
       m_int[i] = ia.m_int[i];
     }
   }
+}
+
+void IntArray::clear() {
+  delete[] m_int;
+  m_int = nullptr;
+  m_size = 0;
+}
+void IntArray::reallocate(int len) {
+  clear();
+  init(len);
+}
+void IntArray::resize(int len) {
+  if (len == m_size)
+    return;
+
+  if (len <= 0) {
+    clear();
+    return;
+  }
+  int *data = new int[len];
+
+  if (m_size > 0) {
+    int elementsToCopy = len > m_size ? m_size : len;
+    for (int i = 0; i < elementsToCopy; ++i) {
+      data[i] = m_int[i];
+    }
+  }
+  delete[] m_int;
+  m_int = data;
+  m_size = len;
+}
+void IntArray::insertBefore(int value, int idx) {
+  assertIndex(idx);
+  int *data = new int[m_size + 1];
+  for (int b = 0; b < idx; ++b) {
+    data[b] = m_int[b];
+  }
+  data[idx] = value;
+  for (int a = idx; a < m_size; ++a) {
+    data[a + 1] = m_int[a];
+  }
+
+  delete[] m_int;
+  m_int = data;
+  m_size += 1;
+}
+void IntArray::remove(int idx) {
+  assertIndex(idx);
+  if (m_size == 1) {
+    clear();
+    return;
+  }
+
+  int *data = new int[m_size - 1];
+  for (int b = 0; b < idx; ++b) {
+    data[b] = m_int[b];
+  }
+  for (int a = idx; a < m_size; ++a) {
+    data[a - 1] = m_int[a];
+  }
+
+  delete[] m_int;
+  m_int = data;
+  m_size -= 1;
 }
--- lessons/lesson9/IntArray.h
+++ lessons/lesson9/IntArray.h
@@ -6,13 +6,24 @@
 #define IVA_INTARRAY_H
 
 #include <iostream>
+#include <cassert>
 class IntArray {
   int *m_int = nullptr;
   int m_size = 0;
+  void init(int size);
+  void assertIndex(int idx) { assert(idx >= 0 && idx <= m_size && "IntArray: subscript index too large"); }
  public:
   explicit IntArray(int size);
   IntArray(const IntArray &ia);
   ~IntArray();
+  int getLength() { return m_size; }
+  void clear();
+  void reallocate(int len);
+  void resize(int len);
+  void insertBefore(int value, int idx);
+  void insertAtBeginning(int value) { insertBefore(value, 0); };
+  void insertAtEnd(int value) { insertBefore(value, m_size); };
+  void remove(int idx);
   int &operator[](const int idx);
   IntArray &operator=(const IntArray &ia);
   friend std::ostream &operator<<(std::ostream &out, const IntArray &ia);
