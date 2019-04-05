29c7e5248adfe5438ea1957e15d0d00acafe947e
Petar Dzhambazov <jamby77@gmail.com>
2019-04-05 20:28:33 +0300

    updated to last task

--- split-words/words.cpp
+++ split-words/words.cpp
@@ -1,90 +1,66 @@
-#include <iostream>
-#include <cstring>
-
+#include<iostream>
+#include<fstream>
 using namespace std;
 
-const int MAX = 256;
 
-char getMostCommonChar(const char *test);
+struct Car {
+  char brand[30];
+  char model[30];
+  float price;
+};
+
 int main() {
-//  int j = 0; // letter increment
-//  int i = 0; // words counter
-//  char temp[MAX] = {'\0'};
-//  char letter;
-//
-//  char str[MAX] = {'\0'};
-//  cin.getline(str, MAX);
-//
-//  int length = static_cast<int>(strlen(str));
-//  char matrix[MAX][MAX] = {'\0'};
-//
-//  for (int k = 0; k < length; k++) {
-//    letter = str[k];
-//
-//    if (letter >= 'A' && letter <= 'z') {
-//      matrix[i][j] = letter; //тук излиза проблема с паметта
-//      j++;
-//    } else {
-//      i++;
-//      j = 0;
-//    }
-//  }
-//
-//  for (int t = 0; t <= i - 1; t++) {
-//    for (int p = t + 1; p <= i; p++) {
-//      if (strcmp(matrix[t], matrix[p]) > 0) {
-//        j = 0;
-//        char c = matrix[t][0];
-//        while (c!='\0') {
-//          temp[j++] = c;
-//          c = matrix[t][j];
-//        }
-//        temp[j] = c;
-//        j = 0;
-//        c = matrix[p][0];
-//        while (c!='\0') {
-//          matrix[t][j++] = c;
-//          c = matrix[p][j];
-//        }
-//        matrix[t][j] = c;
-//        j = 0;
-//        c = temp[0];
-//        while (c!='\0') {
-//          matrix[p][j++] = c;
-//          c = temp[j];
-//        }
-//        matrix[p][j] = c;
-//      }
-//    }
-//  }
-//
-//  for (int t = 0; t <= i; t++) {
-//    cout << matrix[t] << endl;
-//  }
 
-  const char test[] = "abbcc defgghi";
-
-  char m = getMostCommonChar(test);
-
-  std::cout << m << '\n';
-
-  return 0;
-}
-char getMostCommonChar(const char *test) {
-  int letters[MAX] = {0};
-  for (int i = 0; i < strlen(test); ++i) {
-    char t = test[i];
-
-    letters[t] += 1;
+  Car carArray[4];
+  for (int i = 0; i < 4; i++) {
+    cin >> carArray[i].brand >> carArray[i].model >> carArray[i].price;
   }
 
-  char m = '\0';
-  int t = 0;
-  for (char j = 'a'; j <='z'; j++) {
-    if (letters[j] > t) {
-      m = j;
-      t = letters[j];
+  //for (int i = 0; i < 4; i++) {
+  //cout << carArray[i].brand << "  " << carArray[i].model << "  " << carArray[i].price << endl;
+  //}
+  ofstream oFile("text.bin", ios::binary);
+
+  if (!oFile.is_open()) {
+    cout << "error";
+  }
+
+  for (int i = 0; i < 4; i++) {
+    oFile.write((char *) &carArray[i], sizeof(Car));
+  }
+  oFile.close();
+
+  Car tempCar;
+  ofstream textFile("text.txt");
+  ifstream iFile("text.bin", ios::binary);
+
+  int k = 0;
+  while (true) {
+    iFile.read((char *) &tempCar, sizeof(Car));
+    if (k%2==1) {
+      textFile.write((char *) &tempCar, sizeof(Car));
     }
+    if (iFile.eof()) {
+      break;
+    }
+    k++;
   }
-  return m;
+  //mazda cx-5 5000 mazda cx-7 6000 mazda cx-9 7000 mazda 6 5000
+  textFile.close();
+
+  ifstream itextFile("text.txt");
+  Car *cars[10];
+  int i = 0;
+  while (i < k/2) {
+    if (itextFile.eof()) {
+      itextFile.close();
+      break;
+    }
+    Car tempCar2;
+    itextFile.read((char *) &tempCar2, sizeof(Car));
+    cars[i] = &tempCar2;
+    std::cout << tempCar2.brand << ' ' << tempCar2.model << ' ' << tempCar2.price << '\n';
+    i++;
+  }
+  return 0;
 }
